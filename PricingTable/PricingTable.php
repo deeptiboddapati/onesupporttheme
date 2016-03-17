02b253002929ef70bda206f0a7a67156ed0b1588
deeptiboddapati <deeptiboddapati@gmail.com>
2016-03-16 23:44:29 -0500

    Removed Options files
    removed old template files and options files

--- PricingTable/PricingTable.php
+++ PricingTable/PricingTable.php
@@ -1,61 +1,189 @@
 <?php
 
-$options = array();
 
+if( function_exists('acf_add_local_field_group') ):
 
-function create_pricing_table_menu_item(){
-	add_menu_page(
-		"Pricing Table",
-		"Pricing Table Settings",
-		"manage_options",
-		"pricingtable",
-		"pricing_table_menu_ost",
-		'dashicons-chart-pie',
-		'3'
-		);
-	add_option(
-		"pricing_table_ost_options",
-		'',
-		'',
-		'yes'
-		);
+acf_add_local_field_group(array (
+	'key' => 'group_56e9f4f56d864',
+	'title' => 'Product Pricing Table Features',
+	'fields' => array (
+		array (
+			'key' => 'field_56e9f57975b61',
+			'label' => 'Features',
+			'name' => 'features',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => 'field_56e9f59e75b62',
+			'min' => 3,
+			'max' => 10,
+			'layout' => 'table',
+			'button_label' => 'Add Feature',
+			'sub_fields' => array (
+				array (
+					'key' => 'field_56e9f59e75b62',
+					'label' => 'Section Heading',
+					'name' => 'section_heading',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array (
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+					'readonly' => 0,
+					'disabled' => 0,
+				),
+				array (
+					'key' => 'field_56e9f5c975b63',
+					'label' => 'Explanation',
+					'name' => 'explanation',
+					'type' => 'wysiwyg',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array (
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'tabs' => 'all',
+					'toolbar' => 'full',
+					'media_upload' => 0,
+				),
+			),
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'product',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
 
-}
+acf_add_local_field_group(array (
+	'key' => 'group_56e695b2000f4',
+	'title' => 'Product Table',
+	'fields' => array (
+		array (
+			'key' => 'field_56e695c046dac',
+			'label' => 'Name',
+			'name' => 'name',
+			'type' => 'select',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array (
+				'onesupport' => 'OneSupport',
+				'onesecurity' => 'OneSecurity',
+				'oneprotect' => 'OneProtect',
+			),
+			'default_value' => array (
+			),
+			'allow_null' => 0,
+			'multiple' => 0,
+			'ui' => 0,
+			'ajax' => 0,
+			'placeholder' => '',
+			'disabled' => 0,
+			'readonly' => 0,
+		),
+		array (
+			'key' => 'field_56ea28414e27a',
+			'label' => 'Product Name',
+			'name' => 'product_name',
+			'type' => 'post_object',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => array (
+				0 => 'product',
+			),
+			'taxonomy' => array (
+			),
+			'allow_null' => 0,
+			'multiple' => 0,
+			'return_format' => 'object',
+			'ui' => 1,
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'widget',
+				'operator' => '==',
+				'value' => 'acf_widget_52',
+			),
+		),
+		array (
+			array (
+				'param' => 'widget',
+				'operator' => '==',
+				'value' => 'acf_widget_ost_pt_widget',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
 
+endif;
 
+add_filter( 'acfw_include_widgets', 'add_include_widgets_pricingtable_ost' );
 
-add_action('admin_menu', 'create_pricing_table_menu_item');
+function add_include_widgets_pricingtable_ost(){
 
-function pricing_table_menu_ost(){
-	if(!current_user_can('manage_options')){
-		wp_die("You have insufficent permissions to access this page.");
-	}
-	global $options;
-	if(isset($_POST['pricing_table_form_ost_submitted'])){
-		$hidden_field = esc_html($_POST['pricing_table_form_ost_submitted']);
-		if($hidden_field == 'Y'){
-			$sample_txt_ost = esc_html($_POST['sample_txt_ost']);
-			$options[ 'product-name'] = esc_html($_POST['product-name' ]);
-			$options[ 'woocommerce-slug'] = esc_html($_POST['woocommerce-slug' ]);
-			$options[ 'monthly-price'] = esc_html($_POST['monthly-price' ]);
-			$options[ 'onetime-fee'] = esc_html($_POST['onetime-fee' ]);
-			$options[ 'yearly-price'] = esc_html($_POST['yearly-price' ]);
-			$options[ 'description'] = esc_html($_POST['description' ]);
-			$options['sample_txt_ost'] = $sample_txt_ost;
-			$options['last_updated'] = time();
-			update_option("pricing_table_ost_options",$options);
-		}
-	}
-	$options = get_option('pricing_table_ost_options');
-	if($options != ''){
-		$sample_txt_ost = $options['sample_txt_ost'];
-		echo $sample_txt_ost;
-		$options[ 'product-name'];
-		echo $options[ 'woocommerce-slug'];
-		echo $options[ 'monthly-price'];
-		echo $options[ 'onetime-fee'];
-		echo $options[ 'yearly-price'];
-		echo $options[ 'description'];
-	}
-	require('pricingtablemenupage.php');
+	$pricingtable_acfw_widgets_ost = array(
+		array(
+			'title' => 'OneSupport Pricing Table',
+			'description' => 'Display a OneSupport Product Features Table Anywhere!',
+			'slug' => 'ost-pt-widget',
+			'id' => 'Ost_PT_Widget',
+		),
+	);
+	
+	return $pricingtable_acfw_widgets_ost;
+	
 }
