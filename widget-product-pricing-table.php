41bd6998e44c7be01087a1dafbe1dffb8111e83f
deeptiboddapati <deeptiboddapati@gmail.com>
2016-03-16 22:59:17 -0500

    Revert "Pricing Table Custom Fields in Product Post type"
    This reverts commit 9f756666caa83c4604c47b28de05cd65f7b197e2.

--- widget-product-pricing-table.php
+++ widget-product-pricing-table.php
@@ -1,58 +1,35 @@
-
-
 <?php
 
-$post_object = get_field('product_name', $acfw );
-//print_r($post_object);
-if( $post_object ): 
+//get benefits function 
+$titletag= 'h3';
+$oneheading = 'Section 1';
+$outerdivid ="accordion";
+$content = " Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$oneheadingoutput = wrapit_ost('h3', $oneheading);
+$contentoutput = wrapit_ost('div',wrapit_ost('p',$content));
 
-	// override $post
-	 setup_postdata( $GLOBALS['post'] =& $post_object );
 
-$choice = get_the_title();
-if($choice =='OneSupport') {
+$twoheading ="Section 2";
+$twoheadingoutput =wrapit_ost('h3', $twoheading);
+$outeroutputcontent = $oneheadingoutput.$contentoutput.$twoheadingoutput.$contentoutput;
+$outeroutput = wrapit_ost('div', $outeroutputcontent,NULL, 'accordion');
+
+$choice = get_field( 'name', $acfw ); 
+if($choice =='onesupport') {
 	$output = "<h2><span class='support'>One</span>Support</h2>";
 }
-elseif($choice =='OneProtect') {
+elseif($choice =='oneprotect') {
 	$output = "<h2><span class='protect'>One</span>Protect</h2>";
 }
-elseif($choice == 'OneSecurity'){
-$output = "<h2><span class='security'>One</span>Security</h2>";
-
-}
 else{
-	$output = wrapit_ost('h2',$choice);
+$output = "<h2><span class='security'>One</span>Security</h2>";
+$output.=$outeroutput;
 }
+
 echo $output;
-	?>
- 
-    
-<?php
-
-// check if the repeater field has rows of data
-if( have_rows('features') ):
-
- 	// loop through the rows of data
- 	$featuresoutput = '';
-    while ( have_rows('features') ) : the_row();
-
-        // display a sub field value
-        $heading= get_sub_field('section_heading');
-        $heading = wrapit_ost('h3',$heading);
-        $explanation = get_sub_field("explanation");
-        $explanation = wrapit_ost('div',$explanation);
-        $featuresoutput .=$heading.$explanation;
-    endwhile;
-    echo wrapit_ost('div',$featuresoutput,array('accordion'),NULL);
-
-else :
-
-    // no rows found
-
-endif;
-
 ?>
-
-    
-    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-<?php endif; ?>
