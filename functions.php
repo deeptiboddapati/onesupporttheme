9f756666caa83c4604c47b28de05cd65f7b197e2
deeptiboddapati <deeptiboddapati@gmail.com>
2016-03-16 22:59:11 -0500

    Pricing Table Custom Fields in Product Post type
    Put Pricing Table Custom Fields in Product Post Type instead of making
    an options page.
    
    Features populate. Next to get the price data to show up.

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_script( 'jquery', false, array(), false, true );
-	wp_register_script("jqueryui",trailingslashit( get_stylesheet_directory_uri() ).'js/jquery-ui-1.11.4/jquery-ui.js', array('jquery'),NULL, true );
+	wp_register_script("jqueryui",trailingslashit( get_stylesheet_directory_uri() ).'libraries/jquery-ui/jquery-ui.js', array('jquery'),NULL, true );
 	wp_enqueue_script('jqueryui');
 	wp_register_script("pricingaccordion",trailingslashit( get_stylesheet_directory_uri() )."js/pricingtableaccordion.js", array('jqueryui'), NULL, true );
 	wp_enqueue_script("pricingaccordion");
@@ -49,6 +49,24 @@ function wrapit_ost($tag=NULL,$content=NULL, $classes=NULL, $ids=NULL){
 	return $output;
 }
 
-require('PricingTable/PricingTable.php')
+//require('PricingTable/PricingTable.php');
 
+
+
+add_filter( 'acfw_include_widgets', 'add_include_widgets_pricingtable_ost' );
+
+function add_include_widgets_pricingtable_ost(){
+
+	$pricingtable_acfw_widgets_ost = array(
+		array(
+			'title' => 'OneSupport Pricing Table',
+			'description' => 'Display a OneSupport Product Features Table Anywhere!',
+			'slug' => 'ost-pt-widget',
+			'id' => 'Ost_PT_Widget',
+		),
+	);
+	
+	return $pricingtable_acfw_widgets_ost;
+	
+}
 ?>
